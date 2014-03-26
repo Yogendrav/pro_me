def352b240f64e06850c17f7714d2d541feef473
Yogi <yogendra.ogn@gmail.com>
2014-03-26 20:31:46 +0530

    working on project modal form

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ActiveRecord::Base
   attr_accessible :project_title, :created_by, :created_at, :project_details, :starts_at, :ends_at, :status, :invite_people
   has_many :project_managements
   has_many :users, through: :project_managements
+  STATUS = {'Running' => 0, 'Pending' => 1}
 end
